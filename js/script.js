dde5dd7febfc8e7aaa5a389c35450d277124f236
igmand <75315753+igmand@users.noreply.github.com>
2021-01-25 14:35:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,3 +14,9 @@ const counters = document.querySelectorAll('.skills__ratings-counter'),
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
+$("a[href^='#up']").click(function() {
+    var _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+});
